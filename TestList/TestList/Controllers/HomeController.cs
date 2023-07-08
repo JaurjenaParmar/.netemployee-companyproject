f8f9289deb54a3b125a89bd1afad4264d117f2ad
JaurjenaParmar <jaurjenaparmar@gmail.com>
2023-07-08 08:00:58 +0530

    EnableDisableDepartmentTest

---
+++ TestList/TestList/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestList.Models;
+using TestList.repo;
+
+namespace TestList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            department dept = new department();
+            deptCRUD dp = new deptCRUD();
+            dept.dept_list = dp.getDepartments();
+            return View(dept);
+        }
+        [HttpPost]
+        public ActionResult Index(department dept)
+        {
+            deptCRUD dp = new deptCRUD();
+            dept.dept_list = dp.getDepartments();
+            return View(dept);
+        }
+        
+        public ActionResult detail(int id)
+        {
+            department dept = new department();
+            deptCRUD dp = new deptCRUD();
+            dp.getStatus(id);
+            dept.dept_list = dp.getDepartments();
+            return RedirectToAction("Index","Home",dept);
+        }
+
+
+    }
+}
---
+++ TestList/TestList/repo/deptCRUD.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TestList.Models;
+
+namespace TestList.repo
+{
+    public class deptCRUD
+    {
+        static string constr = ConfigurationManager.ConnectionStrings["sqlcon"].ToString();
+        SqlConnection conn = new SqlConnection(constr);
+        public List<department> getDepartments()
+        {
+            try
+            {
+                conn.Open();
+                IList<department> dept_list = SqlMapper.Query<department>(conn, "viewdept").ToList();
+                conn.Close();
+                return dept_list.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        public void getStatus(int id)
+        {
+            try
+            {
+                conn.Open();
+                DynamicParameters param = new DynamicParameters();
+                param.Add("id", id);
+                conn.Execute("toggleIsActive", param, commandType: System.Data.CommandType.StoredProcedure);
+                conn.Close();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
